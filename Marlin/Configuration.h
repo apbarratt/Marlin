748e5314295b50463b7fd71f5f3084fa3307da73
apbarratt <andy@barratt.scot>
2021-05-15 17:51:40 +0100

    Updated with latest PID autotune results.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -689,9 +689,9 @@
     #define DEFAULT_KI_LIST {   1.54,   1.54 }
     #define DEFAULT_KD_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_KP  26.30
-    #define DEFAULT_KI   1.90
-    #define DEFAULT_KD  91.20
+    #define DEFAULT_KP  22.12
+    #define DEFAULT_KI   1.47
+    #define DEFAULT_KD  83.36
   #endif
 #else
   #define BANG_MAX 255    // Limit hotend current while in bang-bang mode; 255=full current
